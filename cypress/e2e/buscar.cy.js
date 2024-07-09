b4ed69fd49af772d3972009f4e0c186df6ba0991
Edenilson Fabiano <20231ITA01GB0011@alunos.ifbaiano.edu.br>
2024-07-09 01:19:33 -0300

    Refatoração e teste de busca

--- cypress/e2e/buscar.cy.js
+++ cypress/e2e/buscar.cy.js
@@ -1,10 +1,28 @@
 /// <reference types="cypress" />
 
 describe('US-012-Funcionalidade: Buscar', () => {
-    it('Cbusca de filmes', () => {
-      cy.visit('http://127.0.0.1:8080/')
+  beforeEach(() => {
+    cy.visit('/')
+  });
+  
+  it('Deve buscar filmes com sucesso', () => {
       cy.get('#search-input').type('the matrix')
         cy.get('#search-button').click()
         cy.get('#search-response').should('contain', 'The Matrix')
-    })
-})
+       });
+
+  it ('Deve buscar filmes com sucesso de uma lista', () => {
+      cy.fixture('filmes').then((filmes) => {
+          cy.get('#search-input').type(filmes[0].titulo)
+          cy.get('#search-button').click()
+          cy.get('#search-response').should('contain', filme[0].titulo)})
+        });
+
+        it ('Deve buscar filmes com sucesso de uma lista inteira', () => {
+            cy.fixture('filmes').each((filmes) => {
+                    cy.get('#search-input').clear().type(filmes.titulo)
+                    cy.get('#search-button').click({force: true})
+                    cy.get('#search-response').should('contain', filmes.titulo)
+              });
+        });
+});
--- cypress/e2e/cadastro.cy.js
+++ cypress/e2e/cadastro.cy.js
@@ -1,15 +1,44 @@
 /// <reference types="cypress" />
 
 describe('US-012-Funcionalidade: Cadastro de membros', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+  
   it('Cadastro de campos obrigatorios', () => {
-    cy.visit('http://127.0.0.1:8080/')
-    cy.get('#signup-firstname').type('AA')
-    cy.get('#signup-lastname').type('Silva')
-    cy.get('#signup-email').type('tes1@test.com')
-    cy.get('#signup-phone').type('11988556611')
-    cy.get('#signup-password').type('Test123456@')
-    cy.get('#signup-button').click()
+   var email = `test${Date.now()}@test.com`
+
+    cy.preencherCadastro('Teste', 'Seset', email, '11999999999', 'T@123456')
     cy.get('#signup-response').should('contain', 'Cadastro realizado com sucesso')
   })
 
+  it('Email já cadastrado', () => {
+    cy.preencherCadastro('Teste', 'Seset', email, '11999999999', 'T@123456')
+    cy.get('#signup-response').should('contain', 'E-mail já cadastrado')
+  })
+
+  it('Nome inválido', () => {
+    cy.preencherCadastro('Teste@', 'Seset', email, '11999999999', 'T@123456')
+    cy.get('#signup-response').should('contain', 'Nome inválido')
+  })
+
+  it('Sobrenome inválido', () => {
+    cy.preencherCadastro('Teste', 'Seset@', email, '11999999999', 'T@123456')
+      cy.get('#signup-response').should('contain', 'Sobrenome inválido')
+  })
+
+  it('Telefone inválido', () => {
+    cy.preencherCadastro('Teste', 'Seset', email, '1199999gf999', 'T@123456')
+    cy.get('#signup-response').should('contain', 'Telefone inválido')
+  })
+
+  it('Senha inválida', () => {
+    cy.preencherCadastro('Teste', 'Seset', email, '11999999999', '1234')
+    cy.get('#signup-response').should('contain', 'Senha inválida')
+  })
+
+  it('Campos obrigatórios não preenchidos', () => {
+    cy.get('#signup-button').click()
+    cy.get('#signup-response').should('contain', 'Preencha todos os campos')
+  })
 })
